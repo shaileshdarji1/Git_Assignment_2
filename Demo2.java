38b04362d8b689b061ae48d3bf79cd8743e6a3cd
shailesh <shailesh.d@simformsolutions.com>
2023-02-20 10:11:04 +0530

    Add new line

--- Demo2.java
+++ Demo2.java
@@ -2,5 +2,6 @@ public Class Demo2{
     public static void main(String args[])
     {
         System.out.println("This is Demo 2 File");
+        System.out.println("This changes made by second branch");
     }
 }
